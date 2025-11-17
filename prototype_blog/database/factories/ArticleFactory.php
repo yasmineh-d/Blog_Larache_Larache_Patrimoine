117a4cc2c0378f0f28266f180443bd3874886d81
yasmineh-d <yassminehadad75@gmail.com>
2025-11-17 16:08:09 +0000

    progress prototype

--- prototype_blog/database/factories/ArticleFactory.php
+++ prototype_blog/database/factories/ArticleFactory.php
@@ -17,7 +17,9 @@ class ArticleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraphs(3, true),
+            'user_id' => User::factory(),
         ];
     }
 }
--- prototype_blog/database/seeders/DatabaseSeeder.php
+++ prototype_blog/database/seeders/DatabaseSeeder.php
@@ -21,5 +21,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $this->call([
+            UserSeeder::class,
+            ArticleSeeder::class,
+            TagSeeder::class,
+            pivoteArticleTagSeeder::class,
+        ]);
     }
 }
