02c987af9678ab0a3fdb9c07246a2e135337372f
yasmineh-d <yassminehadad75@gmail.com>
2025-11-17 15:53:26 +0000

    prototype

--- prototype_blog/app/Models/Article.php
+++ prototype_blog/app/Models/Article.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    // Mass assignable attributes
+    protected $fillable = ['title', 'content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+        // Un article appartient à un utilisateur
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+        // Un article peut avoir plusieurs tags (et vice versa)
+        // Laravel utilise automatiquement la table article_tag
+    }
 }
--- prototype_blog/app/Models/Tag.php
+++ prototype_blog/app/Models/Tag.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    // Mass assignable attributes
+    protected $fillable = ['name'];
+
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class);
+        // Un tag peut être sur plusieurs articles
+    }
 }
